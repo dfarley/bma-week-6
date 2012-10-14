18f3a3ebcbe692dd8b1c23d1c05df17867e6e025
dfarley <david@crossfireconsulting.com>
2012-10-14 14:37:25 -0400

    added initial classes

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		drivers.Driver driver = new drivers.Driver();
+		driver.run();
+	}
+}
---
+++ src/drivers/Driver.java
@@ -0,0 +1,7 @@
+package drivers;
+
+public class Driver {
+	public void run() {
+		System.out.println("running drivers.Driver");
+	}
+}
